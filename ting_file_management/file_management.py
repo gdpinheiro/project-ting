e6e37ba100cfa32eb25278bdf812fcd1736234c0
black <gikigiio@gmail.com>
2022-09-13 16:38:54 -0300

    Req 2

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,12 @@
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    try:
+        if path_file.endswith(".txt"):
+            with open(path_file) as f:
+                split_file = f.read().split("\n")
+                return split_file
+        return sys.stderr.write("Formato inválido\n")
+    except FileNotFoundError:
+        return sys.stderr.write(f"Arquivo {path_file} não encontrado\n")
