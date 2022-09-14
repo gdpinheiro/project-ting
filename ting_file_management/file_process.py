a401b990c46cd189e2d98af1ae1008bf5ff492d9
black <gikigiio@gmail.com>
2022-09-14 16:50:20 -0300

    Req 3 e 4

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -3,8 +3,8 @@ from ting_file_management.file_management import txt_importer
 
 
 def process(path_file, instance):
-    for i in range(len(instance)):
-        if instance.search(i)["nome_do_arquivo"] == path_file:
+    for index in range(len(instance)):
+        if instance.search(index)["nome_do_arquivo"] == path_file:
             return
 
     current_file = txt_importer(path_file)
@@ -15,12 +15,15 @@ def process(path_file, instance):
         "linhas_do_arquivo": current_file,
     }
 
+    instance.enqueue(processed_file)
+
     print(processed_file, file=sys.stdout)
 
 
 def remove(instance):
-    if instance.__len__() == 0:
-        sys.stdout.write("Não há elementos")
+    if len(instance) == 0:
+        print("Não há elementos", file=sys.stdout)
+        return
 
     file_name = instance.dequeue()["nome_do_arquivo"]
 
