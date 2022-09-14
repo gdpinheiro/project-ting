5d719a41ebb4cd9cee1a1c24c2081ea8e5ed59c9
black <gikigiio@gmail.com>
2022-09-14 16:36:29 -0300

    Req 3

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,21 @@
+import sys
+from ting_file_management.file_management import txt_importer
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    for i in range(len(instance)):
+        if instance.search(i)["nome_do_arquivo"] == path_file:
+            return
+
+    current_file = txt_importer(path_file)
+
+    processed_file = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(current_file),
+        "linhas_do_arquivo": current_file,
+    }
+
+    sys.stdout.write(processed_file)
 
 
 def remove(instance):
